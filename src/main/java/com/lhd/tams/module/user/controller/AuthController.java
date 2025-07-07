b393fbe31f138a27ba0258e887d250db4a2df93b
gzp <2139728203@qq.com>
2025-07-07 20:46:24 +0800

    修改密钥

--- src/main/java/com/lhd/tams/module/user/controller/AuthController.java
+++ src/main/java/com/lhd/tams/module/user/controller/AuthController.java
@@ -102,8 +102,6 @@ class RegisterDTO {
     public void setPassword(String password) { this.password = password; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-    public String getAvatarUrl() { return avatarUrl; }
-    public void setAvatarUrl(String avatarUrl) { this.avatarUrl = avatarUrl; }
 }
 
 // 新增DTO
