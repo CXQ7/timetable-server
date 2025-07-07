00e5c210731dec1e0ee0cd6d382f9985d92c64ba
CXQ7 <919136629@qq.com>
2025-07-08 01:44:09 +0800

    test3

--- src/main/java/com/lhd/tams/module/user/model/data/UserDO.java
+++ src/main/java/com/lhd/tams/module/user/model/data/UserDO.java
@@ -1,13 +1,17 @@
 package com.lhd.tams.module.user.model.data;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
 @Data
+@TableName("user")
 public class UserDO {
     private Long id;
     private String username;  // 用户名（用于登录）
     private String password;  // 密码（需加密存储）
     private String role;      // 角色（如 STUDENT/TEACHER）
     private String email;     // 邮箱
+    @TableField("avatar_url")
     private String avatarUrl; // 头像URL
 }
